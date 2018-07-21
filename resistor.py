Fixed rotation, should always rotate resistor to horizontal

resistor.py
@@ -69,11 +69,7 @@ def get_bounding_box(img):
 	width = rect[1][0]
 	height = rect[1][1]
 	angle = rect[2]
-	if width < height:
-	    angle = angle + 90
-	    temp = width
-	    width = height
-	    height = temp
+
 	box = cv2.boxPoints(rect)
 	box = np.int0(box)
 	
@@ -90,24 +86,46 @@ def crop_and_rotate(img, rot_mat, rect):
 	width = rect[1][0]
 	height = rect[1][1]
 	print('Rot Mat', rot_mat)
-	result = cv2.warpAffine(img, rot_mat, (int(width),int(height)), flags=cv2.INTER_LINEAR)
-
-	#cv2.imshow("res", result)
-	#cv2.waitKey()
+	result = cv2.warpAffine(img, rot_mat, (img.shape[1],img.shape[0]), flags=cv2.INTER_LINEAR)
 
 	rows = img.shape[0]
 	cols = img.shape[1]
 
 	delta_width = cols-width
-	#print(delta_width)
+	if delta_width < 0:
+		delta_width = 0
+	print("Delta_W: ", delta_width)
 	delta_height = rows-height
-	#print(delta_height)
+	if delta_height < 0:
+		delta_width = 0
+	print("Delta_H: ", delta_height)
 
-	#print("Rows: ", int(delta_height/2, rows-int(delta_height/2)))
+	print("Rows: ", int(delta_height/2), rows-int(delta_height/2))
+	print("Cols: ", int(delta_width/2), cols-int(delta_width/2))
 
-	#result = result[int(delta_height/2):rows-int(delta_height/2)][int(delta_width/2):cols-int(delta_width/2)]
-	cv2.imshow("test", result)
-	cv2.waitKey()
+
+	result = result[int(delta_height/2):rows-int(delta_height/2), int(delta_width/2):cols-int(delta_width/2)]
+	result = result[10:(result.shape[0]-10), 10:(result.shape[1]-10)]
+	res_center = tuple(np.array(result.shape[1::-1])/2)
+	print("Res Center: ", res_center)
+
+	h = result.shape[0]
+	w = result.shape[1]
+
+	if w < h:
+		rot = cv2.getRotationMatrix2D(res_center, 90, 1.0)
+		cos = abs(rot[0,0])
+		sin = abs(rot[0,1])
+
+		nW = int((h*sin) + (w*cos))
+		nH = int((h*cos) + (w*sin))
+
+		rot[0,2] += (nW/2 - res_center[0])
+		rot[1,2] += (nH/2 - res_center[1])
+
+		result = cv2.warpAffine(result, rot, (nW, nH), flags = cv2.INTER_LINEAR)
+
+
 	return result
 
 def get_horizontal(img):
@@ -126,11 +144,6 @@ def get_horizontal(img):
 	print("Rect Height, Width: ", height, width)
 	print("Angle: ", angle)
 
-	angle_rad = angle*0.0174533
-
-	#new_width = int(math.cos(angle_rad)*height + math.sin(angle_rad)*width)
-	#new_height = int(math.sin(angle_rad)*height + math.cos(angle_rad)*width)
-
 	new_width = cols
 	new_height = rows
 	print("New Height, New Width: ", new_height, new_width)
@@ -142,10 +155,8 @@ def get_horizontal(img):
 	M = np.array([[1,0, x_offset],[0, 1, y_offset]])
 	img = cv2.warpAffine(img,M,(new_width, new_height))
 
-	#cv2.imshow("translate", img)
-	#cv2.waitKey()
-
-	centre = ((new_height)/2.0, (new_width)/2.0)
+	centre = ((new_width-1)/2.0, (new_height-1)/2.0)
+	print("Centre", centre)
 
 	rot_mat = get_rotation_matrix(centre, rect)
 	return crop_and_rotate(img, rot_mat, rect)
@@ -159,6 +170,9 @@ def main():
 	k2 = 10
 
 	img = cv2.imread(os.path.join(os.getcwd(), 'images', 'ours1.jpg'), 1)
+	#res_center = tuple(np.array(img.shape[1::-1])/2)
+	#rot = cv2.getRotationMatrix2D(res_center, 90, 1.0)
+	#img = cv2.warpAffine(img, rot, (img.shape[0],img.shape[1]), flags = cv2.INTER_LINEAR)
 
 	rgbimg = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 	labimg = cv2.cvtColor(img, cv2.COLOR_BGR2Lab)
@@ -201,10 +215,6 @@ def main():
 	plt.imshow(cv2.cvtColor(rotated_img, cv2.COLOR_BGR2RGB))
 
 	plt.show()
-
-	#cv2.imshow("jang", crop_img)
-
-	#cv2.waitKey()
 
 if __name__ == '__main__':
 	main()